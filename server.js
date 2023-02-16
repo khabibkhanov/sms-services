dc710366f6c1e5200e388eec6dd48e8e2f7a26f9
khabibkhanov <hamidulloxon2002@gmail.com>
2023-02-16 16:06:55 +0500

    jwt cofigured

--- server.js
+++ server.js
@@ -1,34 +1,45 @@
-const express = require('express');
-const http = require('http');
-const ReconnectingWebSocket = require('ws');
+// Import necessary modules
+const express = require('express'); // Express is a popular web framework for Node.js
+const http = require('http'); // The HTTP module is built-in to Node.js and provides functionality for creating a basic HTTP server
+const ReconnectingWebSocket = require('ws'); // A WebSocket library that provides automatic reconnection in case of connection failures
 
-const app = express()
+// Create an instance of the Express app
+const app = express();
+
+// Create an HTTP server using the Express app
 const server = http.createServer(app);
-const wss = new ReconnectingWebSocket.Server({ server });
-wss.reconnectInterval = 60000
 
-// Pass the wss instance to locals
+// Create a new WebSocket server using the HTTP server and the ReconnectingWebSocket library
+const wss = new ReconnectingWebSocket.Server({ server });
+
+// Set the reconnect interval to 60 seconds (in case of connection failures)
+wss.reconnectInterval = 60000;
+
+// Pass the wss instance and ReconnectingWebSocket library to the app.locals object, so they can be accessed by other modules
 app.locals.websockets = {
 	wss,
 	ReconnectingWebSocket
-}
+};
+
+// Use the built-in Express middleware for parsing JSON data
 app.use(express.json());
 
-app.use( function (req, res, next)  {
-	res.setHeader('Access-Control-Allow-Origin', '*')
-	res.setHeader('Access-Control-Allow-Headers', '*')
-	next()	
-})
+// Set CORS headers to allow cross-origin requests from any origin and any headers
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+	next();
+});
 
+// Create a simple HTTP route that returns a JSON response with a message
 app.get('/', function(req, res) {
-	res.set('Content-Type', 'application/json')
-	res.send(JSON.stringify('SMS Service'))
-})
+	res.set('Content-Type', 'application/json');
+	res.send(JSON.stringify('SMS Service'));
+});
 
+// Load external modules for handling various routes and functionality
+const modules = require('./src/modules');
+app.use(modules);
 
-// load modules
-const modules = require('./src/modules')
-
-app.use( modules )
-
-server.listen(process.env.PORT || 5000)
+// Start the HTTP server, listening on the specified port (or defaulting to port 5000)
+server.listen(process.env.PORT || 5000);
--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -1,7 +1,10 @@
-const { sign, verify } = require('jsonwebtoken')
-const SECRET_KEY = 'shoptoli'
+// Import the `sign` and `verify` functions from the `jsonwebtoken` library
+const { sign, verify } = require('jsonwebtoken');
 
+// Export an object containing the `sign` and `verify` functions, which use the `jsonwebtoken` library and the secret key to create and verify JSON Web Tokens (JWTs)
 module.exports = {
-	sign: (payload) => sign(payload, SECRET_KEY),
-	verify: (token) => verify(token, SECRET_KEY),
-}
+	// The `sign` function takes a payload (e.g. user ID, email, etc.) and returns a signed JWT
+	sign: (payload) => sign(payload, process.env.JWTSECRETKEY),
+	// The `verify` function takes a JWT and verifies its signature, returning the decoded payload if the signature is valid
+	verify: (token) => verify(token, process.env.JWTSECRETKEY),
+};
